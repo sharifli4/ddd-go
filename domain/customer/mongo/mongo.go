e2ae36f9bb95d14cedbe3a56f085dbc32ccb5127
Kenan <kenansharifov400@gmail.com>
2023-10-27 14:28:33 +0400

    Create mongo domain

---
+++ domain/customer/mongo/mongo.go
@@ -0,0 +1,69 @@
+package mongo
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/shari4ov/ddd-go.git/aggregate"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+type MongoRepository struct {
+	db       *mongo.Database
+	customer *mongo.Collection
+}
+
+// mongoCustomer is an internal type that is used to store a CustomerAggregate inside this repository
+type mongoCustomer struct {
+	ID   uuid.UUID `bson:"id"`
+	Name string    `bson:"name"`
+}
+
+func NewFromCustomer(c aggregate.Customer) mongoCustomer {
+	return mongoCustomer{
+		ID:   c.Person.ID,
+		Name: c.Person.Name,
+	}
+}
+func (m mongoCustomer) ToAggregate() aggregate.Customer {
+	c := aggregate.Customer{}
+	c.Person.ID = m.ID
+	c.Person.Name = m.Name
+	return c
+}
+func New(ctx context.Context, connectionString string) (*MongoRepository, error) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	if err != nil {
+		return nil, err
+	}
+	db := client.Database("ddd")
+	customers := db.Collection("customers")
+	return &MongoRepository{
+		db:       db,
+		customer: customers,
+	}, nil
+}
+func (mr *MongoRepository) Get(id uuid.UUID) (aggregate.Customer, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result := mr.customer.FindOne(ctx, bson.M{"id": id})
+	var c mongoCustomer
+	if err := result.Decode(c); err != nil {
+		return aggregate.Customer{}, err
+	}
+	return c.ToAggregate(), nil
+}
+func (mr *MongoRepository) Add(c aggregate.Customer) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	internal := NewFromCustomer(c)
+	if _, err := mr.customer.InsertOne(ctx, internal); err != nil {
+		return err
+	}
+	return nil
+}
+func (mr *MongoRepository) Update(c aggregate.Customer) error {
+	panic("to implement")
+}
